531a8b7f30203b51fb6b538becf4e7266aca89bf
Eirik Daleng Haukedal <eirikdal@stud.ntnu.no>
2011-04-09 14:01:07 +0200

    test

---
+++ controllers/Marvin/src/ann.py
@@ -0,0 +1,17 @@
+'''
+Created on 8. apr. 2011
+
+@author: hauk184
+'''
+
+class ANN(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
